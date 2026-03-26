d3811e70be433ee8473674911dda00feed65998f
Adrian Gjerstad <adgjerstad@gmail.com>
2026-03-26 11:02:18 -0400

    Add basic Fiber implementation
    Current implementation is not covered by tests, and is not suitable for
    submission to the mainline. The implemented Fiber class acts as a
    higher-level wrapper around ConfigureStack and SwitchStack, supporting
    functions that work closer to how a developer familiar with writing code
    that runs in fibers would expect it.

---
+++ fibers/fiber.cc
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Copyright 2026 Adrian Gjerstad.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// -----------------------------------------------------------------------------
+// loom/fibers/fiber.cc
+// -----------------------------------------------------------------------------
+//
+// Implementation details of loom::Fiber.
+//
+
+#include "fibers/fiber.h"
+
+#include "fibers/stackarena.h"
+#include "fibers/stackswitch.h"
+
+namespace loom {
+
+namespace {
+
+thread_local Fiber* current_fiber = nullptr;
+
+}
+
+Fiber::~Fiber() {
+  if (arena_ == nullptr) {
+    return;
+  }
+
+  state_ = Fiber::State::kDead;
+  stack_ = nullptr;
+  task_ = nullptr;
+  sp_ = nullptr;
+  yield_sp_ = nullptr;
+  arena_->Release(stack_);
+}
+
+void Fiber::Reap(Fiber* fiber) {
+  if (fiber->state_ != Fiber::State::kSuspended &&
+      fiber->state_ != Fiber::State::kDead) {
+    // Do not reap a fiber that is already running.
+    return;
+  }
+
+  fiber->~Fiber();
+}
+
+Fiber* Fiber::GetCurrentFiber() {
+  return current_fiber;
+}
+
+void Fiber::Jump() {
+  // Configure this thread's environment for running this fiber
+  current_fiber = this;
+  state_ = Fiber::State::kRunning;
+
+  // Perform the context switch
+  loom::SwitchStack(sp_, &yield_sp_);
+
+  // Fiber has yielded back, reset the environment
+  if (state_ == Fiber::State::kRunning) {
+    // We only want to update this state if the fiber still has work to do. If
+    // the fiber returns, state will already be marked kDead by the time we get
+    // here.
+    state_ = Fiber::State::kSuspended;
+  }
+  current_fiber = nullptr;
+}
+
+void Fiber::YieldBack() {
+  loom::SwitchStack(yield_sp_, &sp_);
+}
+
+Fiber::Fiber(StackArena* arena, void* stack) : arena_(arena), stack_(stack),
+    state_(Fiber::State::kSuspended) {
+  sp_ = loom::ConfigureStack(stack_, arena_->stack_size(), Fiber::EntryPoint,
+                             this);
+  yield_sp_ = nullptr;
+}
+
+void Fiber::EntryPoint(void* fiber_ptr) {
+  auto* fiber = static_cast<Fiber*>(fiber_ptr);
+
+  // Run task
+  fiber->task_();
+
+  // Prevent returning from this function at all costs.
+  while (true) {
+    fiber->state_ = Fiber::State::kDead;
+    fiber->YieldBack();
+  }
+}
+
+}
+
